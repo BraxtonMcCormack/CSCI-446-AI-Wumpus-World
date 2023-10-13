17c2463721ac7a7e4ae67a0e9f966e253af04ecf
androidorb424 <captlego13@gmail.com>
2023-10-13 14:32:55 -0600

    FOL in temp.py works! I think

--- temp/temp.py
+++ temp/temp.py
@@ -1,13 +1,12 @@
 class Predicate:
-    def __init__(self, name, args):
+    def __init__(self, name, args, is_negated=False):
         self.name = name
         self.args = args
+        self.is_negated = is_negated
 
     def __str__(self):
-        return f"{self.name}({', '.join(self.args)})"
-
-    def __eq__(self, other):
-        return self.name == other.name and self.args == other.args
+        negation = "¬" if self.is_negated else ""
+        return f"{negation}{self.name}({', '.join(self.args)})"
 
 class KnowledgeBase:
     def __init__(self):
@@ -16,80 +15,74 @@ class KnowledgeBase:
     def add_clause(self, clause):
         self.clauses.append(clause)
 
-    def resolve(self, query):
-        while True:
-            new_clauses = []
-            for clause1 in self.clauses:
-                for clause2 in self.clauses:
-                    if clause1 != clause2:
-                        for literal1 in clause1:
-                            for literal2 in clause2:
-                                unifier = self.unify(literal1, literal2)
-                                if unifier is not None:
-                                    new_clause = self.combine_clauses(clause1, clause2, literal1, literal2, unifier)
-                                    if not new_clause:
-                                        return True
-                                    new_clauses.append(new_clause)
-            if not new_clauses:
-                return False
-            self.clauses.extend(new_clauses)
-
-    def unify(self, literal1, literal2):
-        if literal1.name != literal2.name:
+    def resolve(self, clause1, clause2):
+        # Perform resolution between two clauses
+        new_clause = []
+        for pred1 in clause1:
+            for pred2 in clause2:
+                if pred1.name == pred2.name and pred1.is_negated != pred2.is_negated:
+                    unification = self.unify(pred1, pred2)
+                    if unification:
+                        new_clause += [p for p in clause1 + clause2 if p != pred1 and p != pred2]
+                        new_clause += [self.substitute(p, unification) for p in new_clause]
+                        break
+            else:
+                continue
+            break
+        else:
             return None
-        unifier = {}
-        for arg1, arg2 in zip(literal1.args, literal2.args):
+        return new_clause
+
+    def unify(self, pred1, pred2):
+        # Unify two predicates
+        substitution = {}
+        if pred1.name != pred2.name or pred1.is_negated == pred2.is_negated:
+            return None
+        for arg1, arg2 in zip(pred1.args, pred2.args):
             if arg1 != arg2:
                 if arg1.islower() and arg2.islower():
-                    return None
-                if arg1.islower():
-                    unifier[arg1] = arg2
+                    substitution[arg1] = arg2
+                elif arg1.islower():
+                    substitution[arg1] = arg2
+                elif arg2.islower():
+                    substitution[arg2] = arg1
                 else:
-                    unifier[arg2] = arg1
-        return unifier
+                    return None
+        return substitution
 
-    def combine_clauses(self, clause1, clause2, literal1, literal2, unifier):
-        new_clause = []
-        for lit in clause1 + clause2:
-            if lit != literal1 and lit != literal2:
-                new_lit = self.apply_substitution(lit, unifier)
-                new_clause.append(new_lit)
-        if not self.has_tautology(new_clause):
-            return new_clause
-        return None
+    def substitute(self, predicate, substitution):
+        # Substitute variables in a predicate
+        new_args = [substitution[arg] if arg in substitution else arg for arg in predicate.args]
+        return Predicate(predicate.name, new_args, predicate.is_negated)
 
-    def apply_substitution(self, literal, unifier):
-        args = [unifier[arg] if arg in unifier else arg for arg in literal.args]
-        return Predicate(literal.name, args)
-
-    def has_tautology(self, clause):
-        for literal1 in clause:
-            for literal2 in clause:
-                if literal1 != literal2 and literal1 == literal2:
+    def is_inconsistent(self, new_clause):
+        # Check if a new clause is inconsistent with the knowledge base
+        for clause in self.clauses:
+            resolvent = self.resolve(clause, new_clause)
+            if resolvent is not None:
+                if not resolvent or all(p.is_negated for p in resolvent):
                     return True
         return False
 
-    def __str__(self):
-        return ', '.join([str(clause) for clause in self.clauses])
-
-if __name__ == '__main__':
+def main():
     kb = KnowledgeBase()
+
+    # Define the clauses
     kb.add_clause([Predicate("Animal", ["F(x)"]), Predicate("Loves", ["G(x)", "x"])])
-    kb.add_clause([Predicate("¬Loves", ["x", "F(x)"]), Predicate("Loves", ["G(x)", "x"])])
-    kb.add_clause([Predicate("¬Loves", ["y", "x"]), Predicate("¬Animal", ["z"]), Predicate("¬Kills", ["x", "z"])])
-    kb.add_clause([Predicate("¬Animal", ["x"]), Predicate("Loves", ["Jack", "x"])])
+    kb.add_clause([Predicate("Loves", ["x", "F(x)"], is_negated=True), Predicate("Loves", ["G(x)", "x"])])
+    kb.add_clause([Predicate("Loves", ["y", "x"], is_negated=True), Predicate("Animal", ["z"], is_negated=True), Predicate("Kills", ["x", "z"], is_negated=True)])
+    kb.add_clause([Predicate("Animal", ["x"], is_negated=True), Predicate("Loves", ["Jack", "x"])])
     kb.add_clause([Predicate("Kills", ["Jack", "Tuna"]), Predicate("Kills", ["Curiosity", "Tuna"])])
     kb.add_clause([Predicate("Cat", ["Tuna"])])
-    kb.add_clause([Predicate("¬Cat", ["x"]), Predicate("Animal", ["x"])])
-    
-    print("Initial Knowledge Base:")
-    for clause in kb.clauses:
-        print("∧".join(map(str, clause)))
+    kb.add_clause([Predicate("Cat", ["x"], is_negated=True), Predicate("Animal", ["x"])])
+    # kb.add_clause([Predicate("Kills", ["Curiosity", "Tuna"], is_negated=True)])  # Adding the negated query
 
-    result = kb.resolve([Predicate("¬Kills", ["Curiosity", "Tuna"])])
-    
-    print("\nResolution result:", result)
+    test_clause = [Predicate("Kills", ["Curiosity", "Tuna"], is_negated=False)]
+    # Check for inconsistency
+    if kb.is_inconsistent(test_clause):
+        print(f"{' and '.join(map(str, test_clause))} is consistent with the knowledge base.")
+    else:
+        print(f"{' and '.join(map(str, test_clause))} is inconsistent with the knowledge base.")
 
-    print("Updated Knowledge Base:")
-    for clause in kb.clauses:
-        print("∧".join(map(str, clause)))
+if __name__ == "__main__":
+    main()
